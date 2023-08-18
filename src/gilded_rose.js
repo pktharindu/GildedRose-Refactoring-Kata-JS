63efdf73cc76e50ef04704a2ed4bd653bad34bd2
slawek <slawek@t01.pl>
2023-08-18 12:10:00 +0200

    cleanups

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,3 +1,6 @@
+const { When, Otherwise, Always } = require('./strategy');
+const { UpBy1, UpBy2, UpBy3, Nullify, Noop, DownBy1, DownBy2, DownBy4 } = require('./actions');
+
 class Item {
   constructor(name, sellIn, quality){
     this.name = name;
@@ -6,43 +9,7 @@ class Item {
   }
 }
 
-const clamp = (value, min, max) => Math.max(min, Math.min(max, value))
-
-const Updater = (qualityChange, sellInChange = -1, min = 0, max = 50) => (item) => {
-  item.sellIn += sellInChange;
-  item.quality = clamp(item.quality + qualityChange, min, max);
-}
-
-const UpBy1 = Updater(1);
-const UpBy2 = Updater(2);
-const UpBy3 = Updater(3);
-const Nullify = Updater(-Infinity);
-const Noop = Updater(0, 0, -Infinity, Infinity);
-const DownBy1 = Updater(-1);
-const DownBy2 = Updater(-2);
-const DownBy4 = Updater(-4);
-
 const strategySets = [];
-
-const When = (condition) => {
-    return {
-        condition,
-        updater: () => {},
-        then: action => ({
-          condition,
-          updater: action
-        })
-    }
-}
-
-const Otherwise = (updater) => {
-  return {
-    condition: () => true,
-    updater
-  }
-}
-
-const Allways = Otherwise
 
 const registerStrategySet = (condition, strategySet) => {
   strategySets.push({
@@ -54,32 +21,32 @@ const registerStrategySet = (condition, strategySet) => {
 const getStrategySet = (item) => strategySets.find(strategySet => strategySet.condition(item)).strategySet
 
 registerStrategySet(item => item.name === "Aged Brie", [
-  When(sellIn => sellIn > 0).then(UpBy1),
+  When(item => item.sellIn > 0).then(UpBy1),
   Otherwise(UpBy2)
 ]);
 
 registerStrategySet(item => item.name === "Backstage passes to a TAFKAL80ETC concert", [
-  When(sellIn => sellIn > 10).then(UpBy1),
-  When(sellIn => sellIn > 5).then(UpBy2),
-  When(sellIn => sellIn > 0).then(UpBy3),
+  When(item => item.sellIn > 10).then(UpBy1),
+  When(item => item.sellIn > 5).then(UpBy2),
+  When(item => item.sellIn > 0).then(UpBy3),
   Otherwise(Nullify)
 ])
 
 registerStrategySet(item => item.name === "Sulfuras, Hand of Ragnaros", [
-  Allways(Noop)
+  Always(Noop)
 ]);
 
 registerStrategySet(item => item.name === "Conjured Mana Cake", [
-  When(sellIn => sellIn > 0).then(DownBy2),
+  When(item => item.sellIn > 0).then(DownBy2),
   Otherwise(DownBy4)
 ]);
 
 registerStrategySet(() => true, [
-  When(sellIn => sellIn > 0).then(DownBy1),
+  When(item => item.sellIn > 0).then(DownBy1),
   Otherwise(DownBy2)
 ]);
 
-const updateItem = (item) => getStrategySet(item).find(strategy => strategy.condition(item.sellIn)).updater(item);
+const updateItem = (item) => getStrategySet(item).find(strategy => strategy.condition(item)).action(item);
 
 class Shop {
   constructor(items=[]){
---
+++ src/strategy.js
@@ -0,0 +1,18 @@
+const When = (condition) => {
+	return {
+		condition,
+		action: () => {},
+		then: action => ({ condition, action })
+	}
+}
+
+const Otherwise = (action) => {
+	return {
+		condition: () => true,
+		action
+	}
+}
+
+const Always = Otherwise
+
+module.exports = { When, Otherwise, Always }
