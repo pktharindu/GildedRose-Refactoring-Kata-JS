3d3dbc2a509102c504ef1e1fd23bbc7e407878a9
P. K. Tharindu <pktharindu@outlook.com>
2023-08-18 10:48:12 +0530

    Extract the Item to a new file and format

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,67 +1,57 @@
-class Item {
-  constructor(name, sellIn, quality){
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
-}
-
 class Shop {
-  constructor(items=[]){
-    this.items = items;
-  }
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
+    constructor(items = []) {
+        this.items = items;
     }
 
-    return this.items;
-  }
+    updateQuality() {
+        for (const element of this.items) {
+            if (element.name !== 'Aged Brie' && element.name !== 'Backstage passes to a TAFKAL80ETC concert') {
+                if (element.quality > 0) {
+                    if (element.name !== 'Sulfuras, Hand of Ragnaros') {
+                        element.quality = element.quality - 1;
+                    }
+                }
+            } else {
+                if (element.quality < 50) {
+                    element.quality = element.quality + 1;
+                    if (element.name === 'Backstage passes to a TAFKAL80ETC concert') {
+                        if (element.sellIn < 11) {
+                            if (element.quality < 50) {
+                                element.quality = element.quality + 1;
+                            }
+                        }
+                        if (element.sellIn < 6) {
+                            if (element.quality < 50) {
+                                element.quality = element.quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+            if (element.name !== 'Sulfuras, Hand of Ragnaros') {
+                element.sellIn = element.sellIn - 1;
+            }
+            if (element.sellIn < 0) {
+                if (element.name !== 'Aged Brie') {
+                    if (element.name !== 'Backstage passes to a TAFKAL80ETC concert') {
+                        if (element.quality > 0) {
+                            if (element.name !== 'Sulfuras, Hand of Ragnaros') {
+                                element.quality = element.quality - 1;
+                            }
+                        }
+                    } else {
+                        element.quality = element.quality - element.quality;
+                    }
+                } else {
+                    if (element.quality < 50) {
+                        element.quality = element.quality + 1;
+                    }
+                }
+            }
+        }
+
+        return this.items;
+    }
 }
 
-module.exports = {
-  Item,
-  Shop
-}
+module.exports = Shop;
--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,4 +1,5 @@
-const {Shop, Item} = require('../src/gilded_rose');
+const Shop = require('../src/gilded_rose');
+const Item = require('../src/item');
 const fs = require('fs');
 
 describe('Golden master', function () {
